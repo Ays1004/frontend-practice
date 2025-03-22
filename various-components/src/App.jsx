833599731abb9210e158f6766c16818b0e7cfb9d
Ays1004 <yewaleayush@gmail.com>
2025-03-22 22:14:21 +0530

    added google search bar component

---
+++ various-components/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GoogleSearchBar from './components/GoogleSearchBar'
+
+function App() {
+
+  return (
+    <>
+      <GoogleSearchBar></GoogleSearchBar>
+    </>
+  )
+}
+
+export default App
